dfb2fedea723c273b81da33f173af32af4ec358b
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-15 15:44:42 -0800

    fix license headers

--- test/Microsoft.AspNetCore.Sockets.Common.Tests/Formatters/ServerSentEventsMessageFormatterTests.cs
+++ test/Microsoft.AspNetCore.Sockets.Common.Tests/Formatters/ServerSentEventsMessageFormatterTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Text;
 using Microsoft.AspNetCore.Sockets.Tests;
 using Xunit;
 
